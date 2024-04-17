fe4b449ca7310fa061630313038b1769b481743c
NOYON18639 <166986185+NOYON18639@users.noreply.github.com>
2024-04-17 10:31:55 +0600

    Add files via upload

---
+++ shamim.py
@@ -0,0 +1,42 @@
+# WRITTEN BY MD.NOYON
+# MY FACEBOOK:HADI FAOUR
+#------------import---------------#
+from os import system as clr
+#--------------logo----------------#
+logo=''' ________               __      ______                __  
+ / ____/ /_  ____  _____/ /_    /_  __/________ ______/ /__
+/ / __/ __ \/ __ \/ ___/ __/_____/ / / ___/ __ `/ ___/ //_/
+ / /_/ / / / / /_/ (__  ) /_/_____/ / / /  / /_/ / /__/ ,<   
+\____/_/ /_/\____/____/\__/     /_/ /_/   \__,_/\___/_/|_|
+'''
+#-------------clear---------------#
+def clear():
+clr:(clear)
+print(logo)
+print(45*'-')
+print('Facebook:Hadi faour')
+#--------------main--------------#
+def main():
+clear()
+print('[01] YOUTUBE CHANNEL HACK)
+print ('[00] Exit')
+line()
+option input=('[??] CHOOSE MENU:')
+if option in ['1,01']:
+  youtube()
+else:
+      exit('ALLAH HAFEEZ')
+#-----------Youtube-------------#
+is_option
+def YOUTUBE CHANNEL_Hack():
+     = input(f"{Wh}\n Enter Youtube channel link target : {Gr}")  # INPUT YOUTUBE CHANNEL
+    print()
+    print(f' {Wh}============= {Gr}SHOW INFORMATION FOR YOUTUBE CHANNEL LINK{Wh}=============')
+    req_api = requests.get(f"http://youtube channel.is/{channel}")  # API YOUTUBE CHANNELWHOIS.IS
+    link_information= json.loads(req_api.text)
+    time.sleep(2)
+    print(f"{Wh}\n Youtube Channel target         :{Gr}", channel link)
+    print(f"{Wh} Gmail      :{Gr}", link_information["gmail"])
+    print(f"{Wh} Password    :{Gr}",link_information ["password"])
+#---------------end---------------#
+
